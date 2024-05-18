bd01129eb60b1158e76d82881a0e1af5b8ac3e2e
deivideich <david_vl2003@hotmail.com>
2024-05-18 22:44:38 +0000

    basic arm listener

--- interbotix_ws/src/arm_handler/scripts/listener.py
+++ interbotix_ws/src/arm_handler/scripts/listener.py
@@ -1,26 +1,67 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import String
-from interbotix_xs_msgs.msg import *
-from interbotix_xs_msgs.srv import *
+from interbotix_xs_msgs.msg import JointSingleCommand
+from interbotix_xs_msgs.srv import OperatingModes, OperatingModesRequest
 from sensor_msgs.msg import JointState
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.position)
-    
-def listener():
+class interbotix_xs_modules:
+    def __init__(self):
+        self.joint_positions = [0] * 9
+        rospy.Subscriber('/wx250s/joint_states', JointState, self.joint_states_callback)
+        self.velocities_publisher = rospy.Publisher('/wx250s/commands/joint_group', JointGroupCommand, queue_size=10)
+        rate = rospy.Rate(10) # 10hz
+    def joint_states_callback(self,data):
+        self.joint_positions = data.position
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+    def change_mode(self, mode):
+        try:
+            set_mode = rospy.ServiceProxy('/wx250s/set_operating_modes', OperatingModes)
+            req = OperatingModesRequest()
+            req.cmd_type='group'
+            req.name = 'arm'
+            req.mode = mode
+            req.profile_type = 'velocity'
+            req.profile_velocity = 131
+            req.profile_acceleration = 15
+            set_mode(req)
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
 
-    rospy.Subscriber("/wx250s/joint_states", JointState, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    def set_velocities(self,velocities):
+        
+        cmd_send = JointSingleCommand('arm',1.0)
+        print(cmd_send)
+        self.velocities_publisher.publish(cmd_send)
 
 if __name__ == '__main__':
-    listener()
+    rospy.init_node('arm_handler')
+    interbotix = interbotix_xs_modules()
+    interbotix.change_mode('velocity')
+    velocities = [1.0]
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown(): 
+        interbotix.set_velocities(velocities)
+        rate.sleep()
+    rospy.spin()
+    
+# #!/usr/bin/env python3
+# # license removed for brevity
+# import rospy
+# from std_msgs.msg import String
+
+# def talker():
+#     pub = rospy.Publisher('chatter', String, queue_size=10)
+#     rospy.init_node('talker', anonymous=True)
+#     rate = rospy.Rate(10) # 10hz
+#     while not rospy.is_shutdown():
+#         hello_str = "hello world %s" % rospy.get_time()
+#         rospy.loginfo(hello_str)
+#         pub.publish(hello_str)
+#         rate.sleep()
+
+# if __name__ == '__main__':
+#     try:
+#         talker()
+#     except rospy.ROSInterruptException:
+#         pass
